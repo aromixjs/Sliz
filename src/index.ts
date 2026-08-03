9eabae7fe5ad13a7b906ab0d9592d2575fd0adb6
istiuak <istiuak.0@gmail.com>
2026-08-03 19:04:43 +0600

    feat(tokenizer): add view block parsing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Tokenizer } from "@lib/tokenizer/tokenizer";
 import { ServerStartMatcher } from "./matchers/ServerStart";
 import { ServerCodeMatcher } from "./matchers/ServerCode";
 import { ServerEndMatcher } from "./matchers/ServerEnd";
+import { ViewStartMatcher } from "./matchers/ViewStart";
+import { ViewEndMatcher } from "./matchers/ViewEnd";
 
 
 
@@ -10,8 +12,8 @@ const tokenizer = new Tokenizer();
 tokenizer.register(new ServerStartMatcher)
 tokenizer.register(new ServerCodeMatcher)
 tokenizer.register(new ServerEndMatcher)
-
-
+tokenizer.register(new ViewStartMatcher)
+tokenizer.register(new ViewEndMatcher)
 
 const code = `<server>
 const user = await getUser();
@@ -22,17 +24,9 @@ async function updateName(name) {
     await db.users.update(user.id, { name });
 }
 </server>
-
 <view>
-    <div>
-        Hello {user.name}
-    </div>
-
-    <button client:click="edit">
-        Edit
-    </button>
-</view>
-`;
+Hello {user.name}
+</view>`;
 
 const data = tokenizer.tokenize(code);
 
