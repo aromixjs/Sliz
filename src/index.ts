72736758e6d6afae35eb55b870f7ec11fe9e6238
istiuak <istiuak.0@gmail.com>
2026-08-04 09:18:36 +0600

    feat(preprocessor): implement HTML preprocessor for embedded expressions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import { Parser } from "@lib/parser/parser";
+import { PreProcessor } from "@lib/preprocessor/preprocessor";
 import { HtmlChunkMatcher, ServerCodeMatcher, ServerEndMatcher, ServerStartMatcher } from "@lib/tokenizer/matchers";
 import { Tokenizer } from "@lib/tokenizer/tokenizer";
-
-
-
-const tokenizer = new Tokenizer([new ServerStartMatcher(), new ServerCodeMatcher(), new ServerEndMatcher(), new HtmlChunkMatcher()])
-
-
+import { SyntaxKind } from "@lib/tokenizer/types";
 const code = `<server>
 const user = await getUser();
 const myName = "user"
@@ -23,11 +19,24 @@ const data = await loadData()
 Hello {user.name}
 </div>`;
 
-const token = tokenizer.tokenize(code)
-// const parser = new Parser();
+const tokenizer = new Tokenizer([new ServerStartMatcher(), new ServerCodeMatcher(), new ServerEndMatcher(), new HtmlChunkMatcher()])
+const tokens = tokenizer.tokenize(code)
+const parser = new Parser();
+const preprocessor = new PreProcessor()
+for (const token of tokens) {
+
+    if (token.kind === SyntaxKind.HtmlToken) {
+        const chunk = preprocessor.process(token)
+        console.log(chunk);
+        
+        if (chunk.value) parser.write(chunk.value)
+    }
+}
+const htmlAst = parser.end()
+console.log(preprocessor.expressions);
+console.log(preprocessor.errors);
 
 
+console.dir(tokens, { depth: null });
+console.dir(htmlAst, { depth: null });
 
-// parser.write(code);
-
- console.dir(token, { depth: null });
