47595089bff1588eb0ae3265eee54828b03fd4f5
istiuak <istiuak.0@gmail.com>
2026-08-10 12:32:45 +0600

    remove preprocessor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { compile } from "./pipeline/compile";
 export { type CompilerContext, type Diagnostic } from "./pipeline/context";
 
-export * as preprocess from "./preprocessor/preprocess";
 export { tokenize } from "./tokenizer/tokenize";
